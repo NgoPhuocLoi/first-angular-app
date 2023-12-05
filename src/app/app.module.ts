cd26e464d2ccefa6e8a33128c317ce2ea4137a31
NgoPhuocLoi <phuocloi11223@gmail.com>
2023-12-05 07:48:10 +0700

    Creating custom components (manually & with CLI)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { CustomButtonComponent } from './custom-button/custom-button.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, CustomButtonComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
