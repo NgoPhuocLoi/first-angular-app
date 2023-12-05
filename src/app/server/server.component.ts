4540d95f6e7e83f97f9cd731807a7f10f2fc5c26
NgoPhuocLoi <phuocloi11223@gmail.com>
2023-12-05 09:32:54 +0700

    Example of data binding (String interpolation, property binding, event binding & two-way binding)

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId = 1;
+  serverStatus = 'Offline';
+  allowNewServer = false;
+  serverCreationStatus = 'No server is created';
+  serverName = 'Server name';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onServerCreate() {
+    this.serverCreationStatus = 'The server is created!';
+  }
+
+  onServerNameChange(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
