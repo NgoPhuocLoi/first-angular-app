f3bef68a0173439278cfb1139ca169811d0dde71
NgoPhuocLoi <phuocloi11223@gmail.com>
2023-12-05 22:05:08 +0700

    Example of some built-in directives (ngIf, @if, ngStyle, ngClass, ngFor, @for)

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,25 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styleUrl: './server.component.css',
 })
 export class ServerComponent {
   serverId = 1;
-  serverStatus = 'Offline';
-  allowNewServer = false;
-  serverCreationStatus = 'No server is created';
-  serverName = 'Server name';
+  serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
 
-  constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000);
-  }
-
-  onServerCreate() {
-    this.serverCreationStatus = 'The server is created!';
-  }
-
-  onServerNameChange(event: Event) {
-    this.serverName = (<HTMLInputElement>event.target).value;
+  getColor() {
+    return this.serverStatus === 'Online' ? 'green' : 'red';
   }
 }
